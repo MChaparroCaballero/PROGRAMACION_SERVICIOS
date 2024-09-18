d25e52231f8a95c8b8c90598f744c8d76f5a5c59
María <150004106+MChaparroCaballero@users.noreply.github.com>
2024-09-18 10:10:16 +0200

    Add files via upload

---
+++ clase_17_9_2024/src/clase_17_9_2024/Biblioteca.java
@@ -0,0 +1,42 @@
+package clase_17_9_2024;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+	ArrayList<Material> almacenaje = new ArrayList<Material>();
+
+	public Biblioteca(ArrayList<Material> almacenaje) {
+		this.almacenaje = almacenaje;
+	}
+
+	public ArrayList<Material> getAlmacenaje() {
+		return almacenaje;
+	}
+
+	public void setAlmacenaje(ArrayList<Material> almacenaje) {
+		this.almacenaje = almacenaje;
+	}
+
+	public void agregarMaterial(Material e) {
+		almacenaje.add(e);
+		System.out.println("Material agregado con exito");
+	}
+
+	public void listarMaterial() {
+		for (Material i : getAlmacenaje()) {
+			System.out.println(i.mostrarInfo());
+		}
+
+	}
+
+	public void BuscarMaterial(String titulo) throws MaterialNoEncontradoException {
+		for (Material i : almacenaje) {
+			if (i.getTitulo().equalsIgnoreCase(titulo)) {
+				i.mostrarInfo();
+			} else {
+				throw new MaterialNoEncontradoException("Titulo no encontrado");
+			}
+
+		}
+	}
+}
