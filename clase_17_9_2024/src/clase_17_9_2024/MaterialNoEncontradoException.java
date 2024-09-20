cce8f76f58bf2ee395f1e9cfe9e9bb47fdeb574d
María <150004106+MChaparroCaballero@users.noreply.github.com>
2024-09-20 13:46:11 +0200

    Delete clase_17_9_2024 directory

--- clase_17_9_2024/src/clase_17_9_2024/MaterialNoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package clase_17_9_2024;
-
-public class MaterialNoEncontradoException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public MaterialNoEncontradoException(String mensaje) {
-		super(mensaje);
-	}
-}
